3f5cfcf0eff1f24692930891abdbf8bae7ef31d4
KhadijaNaveed57 <mcaf19a0312pucit.edu.pk>
2021-05-19 14:33:43 +0500

    After navigation bar

--- grocery_project/app/src/main/java/com/khadijanaveed/grocerystore/home.java
+++ grocery_project/app/src/main/java/com/khadijanaveed/grocerystore/home.java
@@ -1,22 +1,31 @@
 package com.khadijanaveed.grocerystore;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
 public class home extends AppCompatActivity {
 
+//  for carousel
     private int[] mImage = new int[]{
             R.drawable.cokecar,R.drawable.colddrinks, R.drawable.dryfruits, R.drawable.dryfruitsandveg, R.drawable.fruits, R.drawable.groceryitems, R.drawable.watermelon
     };
-
+// for drawer navigation
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        for carousel
         setContentView(R.layout.activity_home);
         CarouselView carouselView = findViewById(R.id.carousel);
         carouselView.setPageCount(mImage.length);
@@ -27,5 +36,15 @@ public class home extends AppCompatActivity {
 
             }
         });
+//        for drawer navigation
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
