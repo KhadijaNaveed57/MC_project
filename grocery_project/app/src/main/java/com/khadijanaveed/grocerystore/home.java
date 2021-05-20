d4baed3db0c1e00882e328c4d3b68e04a3cb69f5
KhadijaNaveed57 <mcaf19a0312pucit.edu.pk>
2021-05-20 15:29:47 +0500

    navigation bar done

--- grocery_project/app/src/main/java/com/khadijanaveed/grocerystore/home.java
+++ grocery_project/app/src/main/java/com/khadijanaveed/grocerystore/home.java
@@ -1,18 +1,24 @@
 package com.khadijanaveed.grocerystore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.google.android.material.navigation.NavigationView;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
-public class home extends AppCompatActivity {
+import java.util.Objects;
+
+public class home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 //  for carousel
     private int[] mImage = new int[]{
@@ -22,6 +28,7 @@ public class home extends AppCompatActivity {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,5 +53,39 @@ public class home extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+//        for click event of navigation items
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+//        switch (item.getItemId())
+//        {
+//            case R.id.fruits:
+//                intent = new Intent(this,fruits.class);
+//                startActivity(intent);
+//                break;
+////            case R.id.vegetables:
+////                intent = new Intent(this, vegetables.class);
+////                startActivity(intent);
+////                break;
+//        }
+//        drawerLayout.closeDrawer(GravityCompat.START);
+//        return true;
     }
 }
